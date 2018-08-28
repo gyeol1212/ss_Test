3852a7784e3d9ff8f905518a8c132ad66476bd51
SangWoo <sangw0804@likelion.org>
2018-08-28 14:46:43 +0900

    devise 관리자 기본 설정 & 기본적인 company 모델 validation 설정

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,6 +4,20 @@ class Company < ApplicationRecord
     has_one_attached :express_file
     has_one_attached :presentation
 
+    # 유효성 검사 추가
+    # validates :season, presence: true
+    # validates :name, presence: true
+    # validates :name_en, presence: true
+    # validates :service_name, presence: true
+    # validates :service_name_en, presence: true
+    # validates :isRegistered, presence: true
+    # validates :isCorporated, presence: true
+    # validates :co_CEO, presence: true
+    # validates :isBusinessStudent, presence: true
+    # validates :company_overview, presence: true, length: {maximum: 500}
+    # validates :service_overview, presence: true, length: {maximum: 500}
+    
+    
     def self.to_csv(options = {})
         CSV.generate(options) do |csv|
             csv << column_names
