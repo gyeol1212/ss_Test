ba6b6d8f2b228af352643fc5a1014f220179e2f9
SangWoo <sangw0804@likelion.org>
2018-08-28 20:38:34 +0900

    관리자 / 입주자 flow 변화

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,6 @@
 class CompaniesController < ApplicationController
   before_action :is_logged_in?
+  # before_action :is_currect_user?
   before_action :is_admin?, only: [:destroy]
 
   def index
@@ -23,10 +24,14 @@ class CompaniesController < ApplicationController
 
   def create
     @company = Company.new(post_params)
+    @company.user = current_user
     if @company.save
-      redirect_to companies_path
-    else
-      flash[:alert] = @company.errors # 유효성 검사를 통과하지 못할 경우 다시 new_company 페이지로 보냄 with flash
+      redirect_to company_path(current_user.company)
+    else # 유효성 검사를 통과하지 못할 경우 다시 new_company 페이지로 보냄 with flash
+      flash[:alert] = ""
+      @company.errors.each do |field, messages|
+        flash[:alert] += "'#{field}' #{messages} | "
+      end
       redirect_to new_company_path
     end
   end
@@ -60,4 +65,11 @@ class CompaniesController < ApplicationController
         redirect_back(fallback_location: root_path)
       end
     end
+
+    # def is_currect_user?
+    #   unless current_user.company.id
+    #     flash[:alert] = "권한이 없습니다."
+    #     redirect_back(fallback_location: root_path)
+    #   end
+    # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_044822) do
+ActiveRecord::Schema.define(version: 2018_08_28_102937) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 2018_08_28_044822) do
     t.string "files"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_companies_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -78,4 +80,5 @@ ActiveRecord::Schema.define(version: 2018_08_28_044822) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "companies", "users"
 end
